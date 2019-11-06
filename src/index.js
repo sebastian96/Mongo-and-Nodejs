ee0a9afc19a97f8f483c21258e73047745a95ec7
Sebastian Miranda <arqcompu4@gmail.com>
2019-11-05 22:37:17 -0500

    autenticazion de usuarios

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 
 // Initializations
 const app = express();
 require('./database');
+require('./config/passport');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -28,13 +30,17 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
+
 
 
 // Global Variables
 app.use((req, res, next) => {
     res.locals.successMsg = req.flash('successMsg');
     res.locals.errorMsg = req.flash('errorMsg');
+    res.locals.error = req.flash('error');
     next();
 });
 
